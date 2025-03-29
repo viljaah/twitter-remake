88431b1499381ef457bc51824fb9a8fcb64f3b65
viljaah <you@example.com>
2025-03-29 15:15:57 +0100

    things

--- backend/app.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from backend.routes import tweet_routes
-
-app = FastAPI()
-
-# prefix? tags?
-app.include_router(tweet_routes.router, prefix='/tweets', tags=['tweets'])
