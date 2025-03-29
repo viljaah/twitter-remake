2c3af65214a7466414425cf233607d4158eb8a2e
modesta <trakselytemodesta@gmail.com>
2025-03-29 15:17:40 +0100

    added minor code in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-from backend.routes import tweet_routes
+#from routes import tweet_routes
 
 app = FastAPI()
 
 # prefix? tags?
-app.include_router(tweet_routes.router, prefix='/tweets', tags=['tweets'])
+#app.include_router(tweet_routes.router, prefix='/tweets', tags=['tweets'])
+
+@app.get("/")
+def read_root():
+    return {"message": "Twitter Clone API is running!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="localhost", port=8000, reload=True)
