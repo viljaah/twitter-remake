995b0275ae85451a15b0847fc82347f07db4c450
modesta <trakselytemodesta@gmail.com>
2025-03-31 21:49:32 +0200

    imported X.logo

--- front-end/src/pages/auth/login/LoginPage.jsx
+++ front-end/src/pages/auth/login/LoginPage.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import XSvg from "../../../components/svgs/X";
+import { MdOutlineMail } from "react-icons/md";
+import { MdPassword } from "react-icons/md";
 
 const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+		e.preventDefault();
+		loginMutation(formData);
+	};
+
+	const handleInputChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
   return (
-    <div>LoginPage</div>
+    <div >
+			<div>
+				<XSvg className='lg:w-2/3 fill-white' />
+			</div>
+			<div >
+				<form  onSubmit={handleSubmit}>
+					<XSvg className='w-24 lg:hidden fill-white' />
+					<h1 >{"Let's"} go.</h1>
+					<label>
+						<MdOutlineMail />
+						<input
+							type='text'
+							className='grow'
+							placeholder='username'
+							name='username'
+							onChange={handleInputChange}
+							value={formData.username}
+						/>
+					</label>
+
+					<label>
+						<MdPassword />
+						<input
+							type='password'
+							className='grow'
+							placeholder='Password'
+							name='password'
+							onChange={handleInputChange}
+							value={formData.password}
+						/>
+					</label>
+					<button>
+						{isPending ? "Loading..." : "Login"}
+					</button>
+					{isError && <p className='text-red-500'>{error.message}</p>}
+				</form>
+				<div>
+					<p>{"Don't"} have an account?</p>
+					<Link to='/signup'>
+						<button>Sign up</button>
+					</Link>
+				</div>
+			</div>
+		</div>
   )
 }
 
