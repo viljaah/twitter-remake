d40338231e86262eca92fb4fa74318747f6c2f22
viljaah <you@example.com>
2025-04-02 14:48:59 +0200

    stuff

--- front-end/src/pages/home/HomePage.jsx
+++ front-end/src/pages/home/HomePage.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div></div>
+    <div className={styles.parentDiv}>
+      <div className={styles.test}>Whats happening?</div>
+    </div>
   )
 }
 
