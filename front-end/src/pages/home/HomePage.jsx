8ff6dcb453ad6acf5c6f1893c6fdc8b82f035ff2
viljaah <you@example.com>
2025-04-03 12:35:31 +0200

    styling tweets some more

--- front-end/src/pages/home/HomePage.jsx
+++ front-end/src/pages/home/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './HomePage.module.css';
 import PostContainer from '../../components/shared/postComponent';
+import { GoHeart } from "react-icons/go";
 
 const HomePage = () => {
   const [tweets, setTweets] = useState([]);
@@ -25,9 +26,35 @@ const HomePage = () => {
     <div className={styles.parentDiv}>
       <PostContainer />
 
-      {tweets.map((tweet) => (
+      {/* {tweets.map((tweet) => (
         <div key={tweet.id} className={styles.tweetCard}>
           <p>{tweet.content}</p>
+        </div>
+      ))} */}
+
+{tweets.map((tweet) => (
+        <div key={tweet.id} className={styles.tweetCard}>
+          <div className={styles.tweetHeader}>
+            <img
+              src={tweet.userAvatar || 'https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg'}
+              alt="User Avatar"
+              className={styles.avatar}
+            />
+            <div>
+              <span className={styles.username}>{tweet.username || 'Anonymous'}</span>
+              <span className={styles.handle}>
+                @{tweet.handle || 'anonymous'}
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.tweetContent}>
+            {tweet.content}
+          </div>
+
+          <div className={styles.tweetFooter}>
+            <GoHeart className={styles.heartIcon} />
+          </div>
         </div>
       ))}
     </div>
