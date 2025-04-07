f43b3ab72b5ee5fb4fe84d122e0118637c76f08e
modesta <trakselytemodesta@gmail.com>
2025-04-07 14:32:54 +0200

    fixed in patch edit problem, it was CORS problem in app.py

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"], # or have [*]
     allow_headers=["*"],
 )
 
